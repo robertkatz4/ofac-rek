8a5ba0bf6dcc2b526a5a2520a903e2780f079a74
robertkatz4 <robert.katz@successacademies.org>
2019-03-25 11:46:10 -0400

    POC for string to Elasticsearch

--- sdntojson.py
+++ sdntojson.py
@@ -1,12 +1,17 @@
 import xmltodict, json, boto3, os
 from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch, RequestsHttpConnection, TransportError
 import requests
+
+host = 'search-movies-lhd34cp5ng4kxk6uxfmvacymte.us-east-1.es.amazonaws.com'
+path = '/my-index/_doc'
+url = host + path
+region = 'us-east-1'
+headers={'Content-Type': 'application/json'}
 
 
 def put_json(data):
-    host = 'https://search-movies-lhd34cp5ng4kxk6uxfmvacymte.us-east-1.es.amazonaws.com/'
-    path = 'my-index/_doc'
-    region = 'us-east-1'
+
 
     credentials = boto3.Session(
                             aws_access_key_id=os.environ["ACCESS_KEY"],
@@ -28,7 +33,6 @@ def put_json(data):
         }
     }
 
-    headers={'Content-Type': 'application/json'}
     r = requests.post(
                     url,
                     json=data,
@@ -37,13 +41,37 @@ def put_json(data):
     print(r.text)
 
 
-with open('sdn.xml') as fd:
-    doc = xmltodict.parse(fd.read())
-    sample = doc['sdnList']['sdnEntry']
-    count = 0
-    while (count < 2):
-        data = json.dumps(sample[count]) + '\n'
-        # print ('######', data)
-        print (data)
-        put_json(data)
-        count = count + 1
+
+awsauth = AWS4Auth(os.environ["ACCESS_KEY"], os.environ["SECRET_KEY"], region, 'es')
+print ('0')
+es = Elasticsearch(
+    hosts=[{'host': host,
+             'port': 443
+    }],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    headers=headers
+)
+s_data= '{"uid": "36", "lastName": "SUCCESS AIRLINES", "sdnType": "Entity", "programList": {"program": "CUBA"}, "akaList": {"aka": {"uid": "12", "type": "a.k.a.", "category": "strong", "lastName": "AERO-CARIBBEAN"}}, "addressList": {"address": {"uid": "25", "city": "Havana", "country": "Cuba"}}}'
+print ('curl -XPOST search-movies-lhd34cp5ng4kxk6uxfmvacymte.us-east-1.es.amazonaws.com/movies/_doc/1 -d ')
+try:
+    es.index('movies', '_doc', s_data) # how you index your document here
+except TransportError as e:
+    print(e.info)
+# print(es.info())
+
+# curl -XPOST search-movies-lhd34cp5ng4kxk6uxfmvacymte.us-east-1.es.amazonaws.com/movies/_doc/1 -d '{"uid": "173",
+#"lastName": "ANGLO-CARIBBEAN CO., LTD.", "sdnType": "Entity", "programList": {"program": "CUBA"}, "akaList": {"aka": {"uid": "57", "type": "a.k.a.", "category": "strong", "lastName": "AVIA IMPORT"}}, "addressList": {"address": {"uid": "129", "address1": "Ibex House, The Minories", "city": "London", "postalCode": "EC3N 1DY", "country": "United Kingdom"}}}' -H 'Content-Type: application/json'
+
+# with open('sdn.xml') as fd:
+#     doc = xmltodict.parse(fd.read())
+#     sample = doc['sdnList']['sdnEntry']
+#     count = 0
+#     while (count < 2):
+#         data = json.dumps(sample[count]) + '\n'
+#         # print ('######', data)
+#         print (data)
+#         put_json(data)
+#         count = count + 1
